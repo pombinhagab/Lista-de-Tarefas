82df18fb7ec332fe4ee66d3758816529729731e3
Gabriel N. <gabrielnascimento12t@gmail.com>
2025-03-26 15:19:09 -0300

    funcionalidades de filtro

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useState } from "react";
 import "./App.css";
 import Todo from "./components/Todo";
-import TodoForm from './components/TodoForm';
-
+import TodoForm from "./components/TodoForm";
+import Search from "./components/Search";
+import Filter from "./components/Filter";
 
 function App() {
   const [todos, setTodos] = useState([
@@ -23,48 +24,74 @@ function App() {
       text: "Estudar React",
       category: "Estudos",
       isCompleted: false,
-    }
+    },
   ]);
+
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
 
   const addTodo = (text, category) => {
     const newTodos = [
-      ...todos, 
+      ...todos,
       {
-      id: Math.floor(Math.random() * 10000),
-      text,
-      category,
-      isCompleted: false,
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false,
       },
     ];
-
     setTodos(newTodos);
   };
 
   const removeTodo = (id) => {
-    const newTodos = [...todos]
-    const filteredTodos = newTodos.filter(todos => todos.id !== id ? todos : null
-    );
+    const filteredTodos = todos.filter((todo) => todo.id !== id);
     setTodos(filteredTodos);
-
   };
 
   const completeTodo = (id) => {
-    const newTodos = [...todos]
-    newTodos.map((todos) => todos.id === id ? todos.isCompleted = !todos.isCompleted : todos)
+    const newTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+    );
     setTodos(newTodos);
-    };
+  };
 
   return (
-  <div className="app">
-    <h1>Lista de Tarefas</h1>
-    <div className="todo-list">
-      {todos.map((todo) => (
-        <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/> 
-      ))}
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <Search search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort}/>
+      
+      <div className="todo-list">
+        {todos
+          .filter((todo) =>
+            filter === "All"
+              ? true
+              : filter === "Completed"
+              ? todo.isCompleted
+              : !todo.isCompleted
+          )
+          .filter((todo) =>
+            todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .sort((a, b) => 
+            sort === "Asc" 
+              ? a.text.localeCompare(b.text) 
+              : b.text.localeCompare(a.text)
+          )          
+          .map((todo) => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              removeTodo={removeTodo}
+              completeTodo={completeTodo}
+            />
+          ))}
+      </div>
+
+      <TodoForm addTodo={addTodo} />
     </div>
-    <TodoForm addTodo={addTodo}/>
-  </div>
-  )
+  );
 }
-//a
-export default App
+
+export default App;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 
 const Search = ({search, setSearch}) => {
   return (
-    <div>
-      Search
+    <div className="search">
+      <h2>Pesquisar</h2>
+      <input 
+      type="text" 
+      value={search} 
+      onChange={(e) => setSearch(e.target.value)} placeholder="Digite para pesquisar"/>
     </div>
   )
 }
 
 export default Search
-//a
